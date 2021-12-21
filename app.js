508169c7e6b094380fa17a67f4c82aced7c1dcb0
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2021-12-21 20:53:40 +0200

    mongodb log

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const config = require('config')
-const mongoose = require('mongoose');
+const path = require('path')
+const mongoose = require('mongoose')
+
 
 const app = express()
+
+app.use(express.json({ extended: true }))
 
 
 const PORT = config.get('port') || 5000
 
-
 async function start() {
-    try {
-await mongoose.connect(config.get('mongoUrl'), { 
-    
-})
-    } catch (err) {
-        console.log('Server Error', e.message)
-        process.exit(1)
-    }
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+        
+    })
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+  } catch (e) {
+    console.log('Server Error', e.message)
+    process.exit(1)
+  }
 }
 
-app.listen(PORT, () => console.log(`App has been listening on ${PORT}...`))
+start()
